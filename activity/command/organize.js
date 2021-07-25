48570bbab7062353e62e868c48d1a6e48239cf25
Astitva Shrestha <astitvashrestha1@gmail.com>
2021-07-25 14:56:58 +0530

    Added organize and tree code

--- activity/command/organize.js
+++ activity/command/organize.js
@@ -1,5 +1,66 @@
-function organize(path){
-    console.log("organize command executed with path ", path+"\n");
+let fs = require("fs");
+let path = require("path"); 
+
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
+function organize(paths){
+  
+    organisedFiles = path.join(paths, "organized files");
+    if(! fs.existsSync(organisedFiles)){
+    fs.mkdirSync(organisedFiles);
+    }
+    
+    let allentities = fs.readdirSync(paths);
+
+    for(let i=0;i<allentities.length;i++){
+
+        entityName = allentities[i];
+        let fullPath = path.join(paths, entityName);
+        let statsOfAPath = fs.lstatSync(fullPath);
+  
+        if(statsOfAPath.isFile()){
+          
+            let ext = path.extname(fullPath);
+            let basename = path.basename(fullPath);
+       
+            for(let key in types){
+                 let typePath = path.join(organisedFiles, key);
+                 if(! fs.existsSync(organisedFiles)){
+                    fs.mkdirSync(typePath);
+                    }
+               
+                let flag = 0;
+                for(let k=0;k<types[key].length;k++){
+                    if(ext.slice(1) == types[key][k]){
+                        flag = 1;
+                        let destPath =  path.join(typePath, basename);
+                        fs.copyFileSync(fullPath, destPath);
+                    }
+                }
+
+                if(flag = 0){
+                    let typePath = path.join(organisedFiles, "others");
+                    if(! fs.existsSync(typePath)){
+                        fs.mkdirSync(typePath);
+                        }
+                    
+                    let destPath =  path.join(typePath, basename);
+                    fs.copyFileSync(fullPath, destPath);
+                }
+            }
+
+        }
+
+    }
+
+
+    console.log("organize command executed with path ", paths+"\n");
+
     return "Organize executed";
 }
 
--- activity/command/tree.js
+++ activity/command/tree.js
@@ -1,5 +1,24 @@
-function create(path){
-    console.log("tree command executed with path ", path+"\n");
+let fs = require("fs");
+let path = require("path"); 
+
+function create(paths){
+
+    let allentities = fs.readdirSync(paths);
+    let baseFolderName = path.basename(paths);
+
+    console.log("|__", baseFolderName)
+    
+    for(let i=0;i<allentities.length;i++){
+        let entityName = allentities[i];
+        let fullPath = path.join(paths, entityName);
+        let statsOfAPath = fs.lstatSync(fullPath);
+
+        if(statsOfAPath.isFile()){
+            console.log("\t|__", entityName);
+        }
+
+    }
+    console.log("tree command executed with path ", paths+"\n");
     return "Tree executed";
 }
 
